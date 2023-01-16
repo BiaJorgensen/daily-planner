b56692447bf31a56e5b1c09cf161040868971abf
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-01-15 19:51:40 -0600

    Removed past/present/future classes from HTML so they can be added in JS depending on the current hour

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,9 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 
   let currentDay = dayjs();
-  $('#currentDay').text(currentDay.format('dddd, MMMM D YYYY'))
+  $('#currentDay').text(currentDay.format('dddd, MMMM D YYYY'));
+
+  
   
   
 
